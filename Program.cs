15659d76fb42e780313ca79a3924cf0ec26d0cd9
Ryan Whitehead <ryebread9001@gmail.com>
2022-10-04 13:32:05 -0600

    hilo functionality

--- Program.cs
+++ Program.cs
@@ -11,173 +11,90 @@ namespace cse210_01
     {
         static void Main(string[] args)
         {
-            string currentPlayer = "x";
             
-            List<List<String>> matrix = GetNewBoard();
-            //List<string> board = GetNewBoard();
-                
-            int moveCount = 0;
-            while (!IsGameOver(matrix, moveCount))
+            int card = 0;
+            int nextCard = 0;
+            int score = 300;
+            Random rnd = new Random();
+            int num = rnd.Next(14);
+            card = num;
+            while (!IsGameOver(score))
             {
-                DisplayBoard(matrix);
-                int choice = GetMoveChoice(currentPlayer);
-                MakeMove(matrix, choice, currentPlayer);
-                moveCount++;
-                    
-                currentPlayer = GetNextPlayer(currentPlayer);
+                
+                Console.WriteLine($"Current card is: {card}");
+                bool choice = GetMoveChoice(card);
+                
+                score = changeScore(card, nextCard, choice, score);
+                Console.WriteLine($"Score: {score}");
+                card = nextCard;
+                nextCard = changeCard(nextCard);
+                
                 
             }
-            currentPlayer = GetNextPlayer(currentPlayer);
-            DisplayWin(matrix, currentPlayer, moveCount);
+            
+            DisplayWin(score);
             
             
             
         }
 
-        static List<List<string>> GetNewBoard()
+        
+
+        static bool IsGameOver(int score)
         {
-            List<List<string>> board = new List<List<string>>();
-            board.Add(new List<String>());
-            board.Add(new List<String>());
-            board.Add(new List<String>());
+            return (score < 0);
+        }
+
+        
+
+        static void DisplayWin(int score)
+        {
             
-
-
-            for (int i = 1; i <= 3; i ++) {
-                board[0].Add(i.ToString());
-            }
-            for (int i = 4; i <= 6; i ++) {
-                board[1].Add(i.ToString());
-            }
-            for (int i = 7; i <= 9; i ++) {
-                board[2].Add(i.ToString());
-            }
             
-
-            return board;
-        }
-
-        static bool IsGameOver(List<List<string>> board, int moves)
-        {
-            return (IsWinner(board,"x") || IsWinner(board, "o") || moves > 8);
-        }
-
-        static void DisplayBoard(List<List<string>> board)
-        {
-            for (var i = 0; i < 3; i++) {
-                for (var j = 0; j < 3; j++) {
-                    Console.Write($"|{board[i][j]}|");
-                    if (j == 2) {
-                        Console.WriteLine("\n --+--+--");
-                    }
-                }
-                
-            }
-        }
-
-        static void DisplayWin(List<List<string>> board, string currentPlayer, int moves)
-        {
-            DisplayBoard(board);
-            if (moves < 9) {
-                Console.WriteLine($"{currentPlayer} Wins!");
-            } else {
-                Console.WriteLine("CATS GAME");
-            }
             
         }
 
 
-        static bool IsWinner(List<List<string>> board, string player)
+        
+        
+        static bool GetMoveChoice(int card)
         {
-            if (board[0][0] == player && board[0][1] == player && board[0][2] == player) { return true; }
-            if (board[1][0] == player && board[1][1] == player && board[1][2] == player) { return true; }
-            if (board[2][0] == player && board[2][1] == player && board[2][2] == player) { return true; }
-
-            // check columns
-            if (board[0][0] == player && board[1][0] == player && board[2][0] == player) { return true; }
-            if (board[0][1] == player && board[1][1] == player && board[2][1] == player) { return true; }
-            if (board[0][2] == player && board[1][2] == player && board[2][2] == player) { return true; }
-
-            // check diags
-            if (board[0][0] == player && board[1][1] == player && board[2][2] == player) { return true; }
-            if (board[0][2] == player && board[1][1] == player && board[2][0] == player) { return true; }
-
-            return false;
-        }
-
-        static string GetNextPlayer(string currentPlayer)
-        {
-            string nextPlayer = "x";
-
-            if (currentPlayer == "x")
-            {
-                nextPlayer = "o";
-            }
-
-            return nextPlayer;
-        }
-
-        static int GetMoveChoice(string currentPlayer)
-        {
-            Console.Write($"{currentPlayer}'s turn to choose a square (1-9) (0 to quit): ");
+            Console.Write($"Will the next card be higher or lower than {card} (h or l)?: ");
             string move = Console.ReadLine();
-            int choice = int.Parse(move);
-            if (choice == 0) {
-                System.Environment.Exit(1);
+            bool choice;
+            if (move == "h") {
+                choice = true;
+            } else {
+                choice = false;
             }
             return choice;
         }
 
-        static void MakeMove(List<List<string>> board, int choice, string currentPlayer)
+        static int changeCard(int nextCard)
         {
             
-            int index = 0;
-            int indey = 0;
-            switch (choice)
-            {
-                
-                case 1:
-                    index = 0;
-                    indey = 0;
-                    break;
-                case 2:
-                    index = 1;
-                    indey = 0;
-                    break;
-                case 3:
-                    index = 2;
-                    indey = 0;
-                    break;
-                case 4:
-                    index = 0;
-                    indey = 1;
-                    break;
-                case 5:
-                    index = 1;
-                    indey = 1;
-                    break;
-                case 6:
-                    index = 2;
-                    indey = 1;
-                    break;
-                case 7:
-                    index = 0;
-                    indey = 2;
-                    break;
-                case 8:
-                    index = 1;
-                    indey = 2;
-                    break;
-                case 9:
-                    index = 2;
-                    indey = 2;
-                    break;
+            Random rnd = new Random();
+            nextCard = rnd.Next(14);
+            return nextCard;
+            
+        }
+
+        static int changeScore(int card, int nextCard, bool choice, int score) 
+        {
+            if (choice) {
+                if (nextCard < card) {
+                    score -= 75;
+                } else {
+                    score += 100;
+                }
+            } else {
+                if (nextCard > card) {
+                    score -= 75;
+                } else {
+                    score += 100;
+                }
             }
-            if (board[indey][index] == "x" || board[indey][index] == "o") {
-                Console.WriteLine("Spot is taken!!!");
-                return;
-            }
-            board[indey][index] = currentPlayer;
+            return score;
         }
     }    
 }
